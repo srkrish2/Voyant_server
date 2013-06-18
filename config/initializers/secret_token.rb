cf54739d054c621ed8e6864f9740255425857e34
Huaming Rao <huaming.rao@gmail.com>
2013-06-18 11:01:01 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Crowddesign::Application.config.secret_token = 'db3c686bc03d489bae4d5b07c8bf87441177b9d1b5068f708c3b690a24c477a64a8fec79bd9e9096b42f7474097ca91203780e2af98b8f1850326353d78e9fce'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Crowddesign::Application.config.session_store :cookie_store, key: '_crowddesign_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Crowddesign::Application.config.session_store :active_record_store
