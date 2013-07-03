137ce53c57b995d5cf05f6d1fcc1f421f47f3689
Huaming Rao <huaming.rao@gmail.com>
2013-07-03 00:06:59 -0500

    deploy configuration for Paperclip

--- config/deploy.rb
+++ config/deploy.rb
@@ -87,3 +87,13 @@ end
 
 after "deploy:setup", "deploy:make_shared_config_folder"
 after "deploy:assets:symlink", "deploy:link_app_configuration_file"
+
+# Paperclip
+namespace :deploy do
+  desc "build missing paperclip styles"
+  task :build_missing_paperclip_styles, :roles => :app do
+    run "cd #{release_path}; RAILS_ENV=production bundle exec rake paperclip:refresh:missing_styles"
+  end
+end
+
+after("deploy:update_code", "deploy:build_missing_paperclip_styles")
